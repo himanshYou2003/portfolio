3b3092b4b5336dff7e8801972e0b8379bdc16571
himanshu <himanshukumar935437#gmail.com>
2025-07-25 15:01:00 +0530

    insect

--- src/data/index.js
+++ src/data/index.js
@@ -78,7 +78,7 @@ const project = [
   {
     name: "AI-Powered Insect Detection for Healthier Crops",
     description:
-      "Built insect detector for farmer to prevent crops",
+      "Built an insect detection system to help farmers protect their crops.",
     image: insect,
     link: "https://insect-detector.vercel.app/", // Add the link here
   },
